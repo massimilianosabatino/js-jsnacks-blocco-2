392ecf72a5fb998fcc04c4321cff5646d867fe8b
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-02-27 11:52:33 +0100

    first commit

---
+++ JSsnack2/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+//Inserisci un numero, se è pari stampa il numero,
+//se è dispari stampa il numero successivo
+
+let number = 0;
+let userNumber = Number(prompt('Inserisci il numero'));
+
+if (userNumber % 2 === 0) {
+    console.log(userNumber);
+} else (userNumber % /= 0) {
+    console.log(userNumber + 1);
+}
+
+
